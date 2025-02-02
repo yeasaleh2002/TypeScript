fa73be5b1084e9e8798e1f2d4327e3a6f334fd14
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 11:21:06 +0600

    union

--- day1/6_union_and_intersection_type.ts
+++ day1/6_union_and_intersection_type.ts
@@ -1,1 +1,37 @@
 // union type
+// union type --> we can combine multiple types using union type
+
+// union type
+type studentName = string | number;
+const studentName1: studentName = "John";
+const studentName2: studentName = 25;
+
+type studentAddress = string | number;
+const studentAddress1: studentAddress = "Bangladesh";
+const studentAddress2: studentAddress = 25;
+
+
+// intersection type
+// intersection type --> we can combine multiple types using intersection type
+
+// intersection type
+type frontEndDeveloper = { 
+    skills: string[];
+    desgination1: string;
+    experience: number;
+}
+
+type backEndDeveloper = {
+    skills: string[];
+    desgination2: string;
+    experience: number;
+}
+
+type fullStackDeveloper = frontEndDeveloper & backEndDeveloper;
+
+const fullStackDeveloper1: fullStackDeveloper = {
+    skills: ["HTML", "CSS", "JavaScript", "React", "NodeJS", "ExpressJS"],
+    desgination1: "Frontend Developer",
+    desgination2: "Backend Developer",
+    experience: 5,
+};
