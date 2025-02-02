2f30c7a8ec5386ee565928546e06b61fb6d3063d
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 11:21:16 +0600

    union

---
+++ day1/dist/6_union_and_intersection_type.js
@@ -0,0 +1,13 @@
+"use strict";
+// union type
+// union type --> we can combine multiple types using union type
+const studentName1 = "John";
+const studentName2 = 25;
+const studentAddress1 = "Bangladesh";
+const studentAddress2 = 25;
+const fullStackDeveloper1 = {
+    skills: ["HTML", "CSS", "JavaScript", "React", "NodeJS", "ExpressJS"],
+    desgination1: "Frontend Developer",
+    desgination2: "Backend Developer",
+    experience: 5,
+};
