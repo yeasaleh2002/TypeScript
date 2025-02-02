a04c961bf7f2db499656d377e2fa26c32cb3b1df
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 11:31:16 +0600

    ternary op

--- day1/7_ternary_optional_chain_nullish_coalescing_operator.ts
+++ day1/7_ternary_optional_chain_nullish_coalescing_operator.ts
@@ -8,6 +8,11 @@
   const isAdult = age >= 18 ? "Adult" : "Not Adult";
   console.log(isAdult); // Adult
 
+  // nullish coalescing operator
+    // --> nullish coalescing operator is a feature that allows us to assign a default value when the value is null or undefined
+    const name: string = null;
+    const userName = name ?? "John";
+
   // optional chaining
   // --> optional chaining is a feature that allows us to access nested properties without worrying if the property exists or not
 
