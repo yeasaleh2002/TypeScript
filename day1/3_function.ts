9f0ad451d7c0852bea151e5a2bb37e3bc24ab877
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 10:06:41 +0600

    type

---
+++ day1/3_function.ts
@@ -0,0 +1,10 @@
+// types of function 
+// Normal Function
+// Arrow Function
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+let result = add(10, 20);
+console.log(result); 
