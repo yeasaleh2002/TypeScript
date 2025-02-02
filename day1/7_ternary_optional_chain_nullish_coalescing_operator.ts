a8c29a4532fccd88884661665a976ec1999241d6
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 11:23:13 +0600

    union

---
+++ day1/7_ternary_optional_chain_nullish_coalescing_operator.ts
@@ -0,0 +1,9 @@
+{
+  // ternary operator
+  // optional chaining
+  // nullish coalescing operator
+
+  // ternary operator
+  const age: number = 18;
+  const isAdult = age >= 18 ? "Adult" : "Not Adult";
+}
