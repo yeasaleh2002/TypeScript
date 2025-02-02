cd6fac6870eea2ec5c0c8495f1a8b94788a6a93b
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 10:53:02 +0600

    function

--- day1/4-spread_operator_destructuring.ts
+++ day1/4-spread_operator_destructuring.ts
@@ -52,5 +52,9 @@
     const { name, age, address, role, balance, company: { name: companyName, address: companyAddress } } = user;
     console.log(name, age, address, role, balance, companyName, companyAddress);
 
+    // array destructuring
+    const numbers: number[] = [1, 2, 3, 4, 5];
+    const [first, second, ...rest] = numbers;
+    console.log(first, second, rest);
 
 }
