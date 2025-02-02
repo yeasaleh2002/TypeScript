af1978140c3c956b5daccc1ca801b89352b8fe99
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 10:07:30 +0600

    function

--- day1/3_function.ts
+++ day1/3_function.ts
@@ -2,9 +2,18 @@
 // Normal Function
 // Arrow Function
 
+// Normal Function
 function add(a: number, b: number): number {
     return a + b;
 }
 
 let result = add(10, 20);
 console.log(result); 
+
+
+// Arrow Function
+const addArrow = (a: number, b: number): number => {
+    return a + b;
+}
+let resultArrow = addArrow(10, 20);
+console.log(resultArrow);
