673a375124581ee4af35b1196c3687cb0c898f84
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 11:45:56 +0600

    nullable

--- day1/dist/7_ternary_optional_chain_nullish_coalescing_operator.js
+++ day1/dist/7_ternary_optional_chain_nullish_coalescing_operator.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 {
     // ternary operator
     // optional chaining
@@ -6,4 +7,18 @@
     // ternary operator
     const age = 18;
     const isAdult = age >= 18 ? "Adult" : "Not Adult";
+    console.log(isAdult); // Adult
+    // nullish coalescing operator
+    // --> nullish coalescing operator is a feature that allows us to assign a default value when the value is null or undefined
+    const name = null;
+    const name1 = undefined;
+    const userName = name !== null && name !== void 0 ? name : "John";
+    const userName1 = name1 !== null && name1 !== void 0 ? name1 : "Steve";
+    console.log(userName); // John
+    console.log(userName1); // Steve
+    const student = {
+        name: "John",
+        age: 25,
+    };
+    console.log((_a = student.address) === null || _a === void 0 ? void 0 : _a.city); // undefined
 }
---
+++ day1/dist/8_never_unknown_nullableType.js
@@ -0,0 +1,32 @@
+"use strict";
+{
+    // nullable type
+    // --> nullable type is a feature that allows us to assign a null value to a variable
+    const searchName = (name) => {
+        if (name === null) {
+            return "Name not found";
+        }
+        else {
+            return `Name: ${name}`;
+        }
+    };
+    console.log(searchName("John")); // Name: John
+    console.log(searchName(null)); // Name not found
+    // unknown type
+    // --> unknown type is a feature that allows us to assign any type of value to a variable
+    const getSpeedInMeterPerSecond = (speed) => {
+        if (typeof speed === "number") {
+            return speed;
+        }
+        else {
+            return "Speed is unknown";
+        }
+    };
+    console.log(getSpeedInMeterPerSecond(100)); // 100
+    console.log(getSpeedInMeterPerSecond("100")); // Speed is unknown
+    // never type
+    // --> never type is a feature that allows us to return never type from a function
+    const throwError = (message) => {
+        throw new Error(message);
+    };
+}
