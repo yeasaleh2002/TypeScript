299f8046b415467a3ddf7e042de03174e34d4df4
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 10:13:53 +0600

    function

--- day1/3_function.ts
+++ day1/3_function.ts
@@ -17,3 +17,22 @@ const addArrow = (a: number, b: number): number => {
 }
 let resultArrow = addArrow(10, 20);
 console.log(resultArrow);
+
+
+// object type function --> but it should called as a method
+const objectUser = {
+    name: "John",
+    balance: 500,
+    addBalance: function(amount: number): number {
+        return this.balance + amount;
+    },
+    addBalanceNew(amount: number): string {
+        return this.balance + amount + " USD";
+    }
+}
+
+
+let arr : number[] = [1, 2, 3, 4, 5];
+const newArr = arr.map((value) => {
+    return value * 2;
+});
