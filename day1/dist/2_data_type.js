b80c469db23d56c4971593dc1c12127320d6f64a
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 09:48:41 +0600

    type

--- day1/dist/2_data_type.js
+++ day1/dist/2_data_type.js
@@ -25,11 +25,6 @@ let x = undefined;
 let y = null;
 // symbol
 let sym1 = Symbol('key1');
-// object
-let person = {
-    name: "John",
-    age: 25
-};
 // array
 let fruits = ['apple', 'banana', 'mango'];
 let numbers = [1, 2, 3, 4, 5];
@@ -37,3 +32,14 @@ let students = [{ name: "John", age: 25 }, { name: "Steve", age: 30 }];
 // tuple
 // tuple --> array --> order --> type of values --> fixed length --> fixed type --> fixed order
 let employee = [1, 'Steve'];
+// object
+let person = {
+    name: "John",
+    age: 25
+};
+// reference type --> object
+let user = {
+    name: "John",
+    age: 25,
+    address: "Bangladesh"
+};
