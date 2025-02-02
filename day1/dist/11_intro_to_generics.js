8ac9fa73a903f24784d2c06dab00996808aad3e5
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 17:04:00 +0600

    type assertion

---
+++ day1/dist/11_intro_to_generics.js
@@ -0,0 +1,21 @@
+"use strict";
+// generics type
+// generics type is a feature that allows us to create reusable components
+// const rollNumbers : number[] = [10, 20, 30, 40, 50];
+// using generics
+const rollNumbers = [10, 20, 30, 40, 50];
+// const mentors: string[] = ["John", "Steve", "Bill", "Ravi"];
+// using generics
+const mentors = ["John", "Steve", "Bill", "Ravi"];
+// const booleanValues: boolean[] = [true, false, true, false];
+// using generics
+const booleanValues = [true, false, true, false];
+const numbers3 = [10, 20, 30, 40, 50];
+// for array of objects
+const user2 = [
+    { name: "John", age: 25, address: "Bangladesh", role: "Admin", balance: 500 },
+    { name: "Steve", age: 30, address: "USA", role: "User", balance: 400 },
+    { name: "Bill", age: 35, address: "UK", role: "Admin", balance: 600 },
+    { name: "Ravi", age: 40, address: "India", role: "User", balance: 300 },
+];
+const studentInfo = ["John", 25];
