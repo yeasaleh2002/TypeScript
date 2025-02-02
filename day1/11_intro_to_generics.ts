1155bb556635199cb7a99f12aca411794ba262ea
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 17:01:43 +0600

    type assertion

--- day1/11_intro_to_generics.ts
+++ day1/11_intro_to_generics.ts
@@ -14,6 +14,20 @@ const mentors: Array<string> = ["John", "Steve", "Bill", "Ravi"];
 const booleanValues: Array<boolean> = [true, false, true, false];
 
 // generics reuseable
-type genericsArray<param> = Array<param>;
+type genericsArray<T> = Array<T>;
 
 const numbers3: genericsArray<number> = [10, 20, 30, 40, 50];
+
+// for array of objects
+const user2: genericsArray<{
+  name: string;
+  age: number;
+  address: string;
+  role: string;
+  balance: number;
+}> = [
+  { name: "John", age: 25, address: "Bangladesh", role: "Admin", balance: 500 },
+  { name: "Steve", age: 30, address: "USA", role: "User", balance: 400 },
+  { name: "Bill", age: 35, address: "UK", role: "Admin", balance: 600 },
+  { name: "Ravi", age: 40, address: "India", role: "User", balance: 300 },
+];
