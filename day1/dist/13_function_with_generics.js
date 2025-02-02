b29a36143799021e242547f3f0f9a4d8587be58c
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 17:51:21 +0600

    generic

--- day1/dist/13_function_with_generics.js
+++ day1/dist/13_function_with_generics.js
@@ -10,3 +10,9 @@ const array1 = createArray("John");
 console.log(array1); // [ 'John' ]
 const array2 = createArrayWithGenerics("Steve");
 console.log(array2); // [ 'Steve' ]
+// generics with multiple types
+const createTuple = (param1, param2) => {
+    return [param1, param2];
+};
+const tuple1 = createTuple("John", 25);
+console.log(tuple1); // [ 'John', 25 ]
