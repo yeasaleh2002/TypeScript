9e1b223cde1e8e910c0c5b91b0990413d632e72b
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-02 17:36:21 +0600

    generic

--- day1/dist/12_generic_with_interface.js
+++ day1/dist/12_generic_with_interface.js
@@ -1,1 +1,36 @@
 "use strict";
+;
+const developer1 = {
+    name: "John",
+    computer: {
+        processor: "Intel",
+        memory: "16GB",
+        storage: "1TB",
+    },
+    smartWatch: {
+        brand: "Apple",
+        price: 500,
+    },
+    mobile: {
+        brand: "Samsung",
+        price: 1000,
+    },
+    IsEmployed: true,
+};
+const developer2 = {
+    name: "Steve",
+    computer: {
+        processor: "AMD",
+        memory: "8GB",
+        storage: "512GB",
+    },
+    smartWatch: {
+        brand: "Samsung",
+        price: 300,
+    },
+    mobile: {
+        brand: "Apple",
+        price: 1200,
+    },
+    IsEmployed: { company: "Google", salary: 100000 },
+};
