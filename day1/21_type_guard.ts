46d15462d319312a0ccadc6e69486f490010cac9
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-03 11:12:16 +0600

    OOP

---
+++ day1/21_type_guard.ts
@@ -0,0 +1,17 @@
+// type guard
+// what is type guard?
+// --> type guard is a way to check the type of a variable in typescript
+
+// type guard with typeof
+// --> typeof is a way to check the type of a variable in typescript
+
+// type guard with instanceof
+// --> instanceof is a way to check the type of a variable in typescript
+
+// type guard with in
+// --> in is a way to check the type of a variable in typescript
+
+// type guard with is
+// --> is is a way to check the type of a variable in typescript
+
+
