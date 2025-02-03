e42d249e72e9126f46bc82300271da15e016b4af
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-03 07:59:37 +0600

    OOP

--- day1/10_interface.ts
+++ day1/10_interface.ts
@@ -1,31 +1,33 @@
-// interface
-// interface is a way to define a type in typescript
+{
+  // interface
+  // interface is a way to define a type in typescript
 
-// interface
-interface Student2 {
+  // interface
+  interface Student2 {
     name: string;
     age: number;
     address: string;
     balance: number;
-}
+  }
 
-type withRole = Student2 & { role: string };
+  type withRole = Student2 & { role: string };
 
-interface withRole2 extends Student2 {
+  interface withRole2 extends Student2 {
     role: string;
-}
+  }
 
-const student3: withRole = {
+  const student3: withRole = {
     name: "John",
     age: 25,
     address: "Bangladesh",
     balance: 500,
     role: "Admin",
-};
+  };
 
-// interface in array
-interface NumberArray2 {
+  // interface in array
+  interface NumberArray2 {
     [index: number]: number;
-}
+  }
 
-const numbers2: NumberArray2 = [10, 20, 30, 40, 50];
+  const numbers2: NumberArray2 = [10, 20, 30, 40, 50];
+}
