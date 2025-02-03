f0c271279a5b4f10f28123466a966ede6a30af2f
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-03 07:09:43 +0600

    mapped types

---
+++ day1/17_mapped_types.ts
@@ -0,0 +1,15 @@
+// mapped type
+// what is mapped type?
+// --> mapped type is a feature that allows us to create a new type from an existing type
+
+// syntax
+/*
+ type mappedType = {
+  [P in keyof type]: type[P];
+}
+P - property
+keyof - keyword
+type - type 
+*/
+
+// Example 1
