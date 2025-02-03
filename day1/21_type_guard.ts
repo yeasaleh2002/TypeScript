58333d5fd8ca7b51b0d1d8ac52470c76a2726919
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-03 11:22:16 +0600

    OOP

--- day1/21_type_guard.ts
+++ day1/21_type_guard.ts
@@ -14,4 +14,42 @@
 // type guard with is
 // --> is is a way to check the type of a variable in typescript
 
+//  example of type guard with typeof
+
+const getSpeedInMeterPerSecond = (speed: unknown) => {
+  if (typeof speed === "number") {
+    return speed;
+  } else {
+    return "Speed is unknown";
+  }
+};
+
+console.log(getSpeedInMeterPerSecond(100)); // 100
+
+
+//  example of type guard with instanceof
+
+class Animal1 {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+class Dog1 extends Animal1 {
+  breed: string;
+  constructor(name: string, breed: string) {
+    super(name);
+    this.breed = breed;
+  }
+}
+
+class Cat1 extends Animal1 {
+  breed: string;
+  constructor(name: string, breed: string) {
+    super(name);
+    this.breed = breed;
+  }
+}
+
 
