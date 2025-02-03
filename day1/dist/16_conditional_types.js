f14e1866b0fd1de7d1473c86400fd40789f78cc7
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-03 07:22:15 +0600

    mapped types

--- day1/dist/16_conditional_types.js
+++ day1/dist/16_conditional_types.js
@@ -1,1 +1,12 @@
 "use strict";
+// conditional type
+// what is conditional type?
+// --> conditional type is a feature that allows us to create a type that depends on another type
+const checkType1 = "String";
+const checkType2 = "Not String";
+console.log(checkType1); // String
+console.log(checkType2); // Not String
+const checkType3 = "Not Number";
+const checkType4 = "Number";
+console.log(checkType3); // Not Number
+console.log(checkType4); // Number
--- day1/dist/17_mapped_types.js
+++ day1/dist/17_mapped_types.js
@@ -1,1 +1,10 @@
 "use strict";
+// mapped type
+// what is mapped type?
+// --> mapped type is a feature that allows us to create a new type from an existing type
+const person1 = {
+    name: "John",
+    age: 25,
+};
+// person.name = "Steve"; // Cannot assign to 'name' because it is a read-only property.            
+console.log(person1.name); // John
