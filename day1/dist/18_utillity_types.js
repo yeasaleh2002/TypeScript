13fff377d920b736569c11171239bab1889bcd11
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-03 07:32:26 +0600

    OOP

--- day1/dist/18_utillity_types.js
+++ day1/dist/18_utillity_types.js
@@ -1,1 +1,7 @@
 "use strict";
+// Utillity types 
+// Utillity types are the predefined generic types that are used to manipulate the types in TypeScript.
+const student4 = {
+    name: "John",
+};
+console.log(student4); // { name: 'John' }
