3440524eaa787e69f65ecfd81d91ca9d0171473d
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-03 08:01:07 +0600

    OOP

--- day1/20_inheritance.ts
+++ day1/20_inheritance.ts
@@ -35,3 +35,30 @@ class Student2 extends Parent {
     }
 }
 
+const student5 = new Student2("John", 25, "Bangladesh", "Admin", 500);
+student5.display();
+
+// output   
+// Name: John, Age: 25, Address: Bangladesh 
+
+class Teacher extends Parent {
+    public subject: string;
+    public salary: number;
+    
+    constructor(name: string, age: number, address: string, subject: string, salary: number) {
+        super(name, age, address);
+        this.subject = subject;
+        this.salary = salary;
+    }
+    
+    display() {
+        super.display();
+        console.log(`Subject: ${this.subject}, Salary: ${this.salary}`);
+    }
+}
+
+const teacher = new Teacher("Steve", 30, "USA", "Math", 1000);
+teacher.display();
+
+// output
+// Name: Steve, Age: 30, Address: USA, Subject: Math, Salary: 1000
