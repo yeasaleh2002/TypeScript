0075de8c5c3a6ebe0ee039d4f44684c569793475
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-03 06:45:37 +0600

    asynchronous

--- day1/15_asynchronous.ts
+++ day1/15_asynchronous.ts
@@ -2,3 +2,31 @@
 
 // what is asynchronous typescript?
 // // --> asynchronous typescript is a way to write asynchronous code in typescript 
+
+// promise
+// what is promise?
+// --> promise is a way to handle asynchronous code in javascript
+
+// simulate 
+// what is simulate?
+// --> simulate is a way to create a fake data or fake function in javascript
+
+
+const createPromise = () : Promise<string> => {
+    return new Promise<string>((resolve, reject) => {
+        const data : string = "Hello World";
+        if(data){
+            resolve(data);
+        } else {
+            reject("No Data Found");
+        }
+    });
+};
+
+// calling create promise function 
+const showData = async () => {
+    const data: string = await createPromise();
+    console.log(data); // Hello World
+}
+
+showData();
