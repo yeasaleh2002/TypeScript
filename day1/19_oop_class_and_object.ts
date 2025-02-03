7bf7e1ae7a118ee83e540b5fa0a75bce48b08251
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-03 07:32:18 +0600

    OOP

---
+++ day1/19_oop_class_and_object.ts
@@ -0,0 +1,34 @@
+// OOP
+
+// what is object oriented programming?
+// Object-oriented programming (OOP) is a programming paradigm based on the concept of "objects", which can contain data, in the form of fields (often known as attributes or properties), and code, in the form of procedures (often known as methods).
+
+// four principles of OOP
+// 1. Encapsulation
+// 2. Inheritance
+// 3. Polymorphism
+// 4. Abstraction
+
+// Encapsulation
+// what is encapsulation?
+// --> encapsulation is a way to restrict access to some of the object's components
+
+// Inheritance
+// what is inheritance?
+// --> inheritance is a way to create a new class using the properties and methods of an existing class
+
+// Polymorphism
+// what is polymorphism?
+// --> polymorphism is a way to perform a single action in different ways
+
+// Abstraction
+// what is abstraction?
+// --> abstraction is a way to hide the implementation details and show only the functionality
+
+// class and object
+// what is class?
+// --> class is a blueprint for creating objects
+
+// what is object?
+// --> object is an instance of a class
+
