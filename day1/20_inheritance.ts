3b6fc83c8bb2cce29971be700793b5f7bb978798
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-02-03 07:58:21 +0600

    OOP

--- day1/20_inheritance.ts
+++ day1/20_inheritance.ts
@@ -2,32 +2,36 @@
 // what is inheritance?
 // --> inheritance is a way to create a new class using the properties and methods of an existing class
 
-class Student2 {
-  public name: string;
-  public age: number;
-  public address: string;
-  public role: string;
-  public balance: number;
-
-  constructor(
-    name: string,
-    age: number,
-    address: string,
-    role: string,
-    balance: number
-  ) {
-    this.name = name;
-    this.age = age;
-    this.address = address;
-    this.role = role;
-    this.balance = balance;
-  }
-
-  display() {
-    console.log(
-      `Name: ${this.name}, Age: ${this.age}, Address: ${this.address}, Role: ${this.role}, Balance: ${this.balance}`
-    );
-  }
+// inheritance
+class Parent {
+    public name: string;
+    public age: number;
+    public address: string;
+    
+    constructor(name: string, age: number, address: string) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+    
+    display() {
+        console.log(`Name: ${this.name}, Age: ${this.age}, Address: ${this.address}`);
+    }
 }
 
+class Student2 extends Parent {
+    public role: string;
+    public balance: number;
+    
+    constructor(name: string, age: number, address: string, role: string, balance: number) {
+        super(name, age, address); // super keyword is used to call the constructor of the parent class
+        this.role = role;
+        this.balance = balance;
+    }
+    
+    display() {
+        super.display();
+        console.log(`Role: ${this.role}, Balance: ${this.balance}`);
+    }
+}
 
